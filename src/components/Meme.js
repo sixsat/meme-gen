a43f00236222a5ed1d1b705cdf0f3fc2fbed9a54
chana sat <77387934+sixsat@users.noreply.github.com>
2022-05-27 22:33:58 +0700

    Add css styles. Add state and event handler in Meme.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,46 @@
 export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    
     return (
         <main>
             <div className="form">
-                <input type="text" placeholder="Top text" className="form-input" autocomplete="off" />
-                <input type="text" placeholder="Bottom text" className="form-input" autocomplete="off" />
+                <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form-input"
+                    autocomplete="off"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form-input"
+                    autocomplete="off"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
                 <button className="form-btn">Get a new meme image 🖼️</button>
             </div>
-            <img src="" alt="" className="meme-image" />
+            <div className="meme">
+                <img src={meme.randomImage} alt="" className="meme-image" />
+                <h2 className="meme-text top">{meme.topText}</h2>
+                <h2 className="meme-text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     );
 }
